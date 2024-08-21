08b8a51bfd590e43f29b781ad10c49c95c1563cd
Suresh Shrestha <sureshcstha@gmail.com>
2024-08-21 10:59:07 -0600

    remove unused variable initialItems

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
-
 function App() {
   const [items, setItems] = useState([]);
 

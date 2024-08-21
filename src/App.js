7f93d68e775267513788a5b8bcff7f628a4d2408
Suresh Shrestha <sureshcstha@gmail.com>
2024-08-21 16:43:29 -0600

    calculate stats using derived state

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <Logo />
       <Form onAddItems={handleAddItems}/>
       <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem}/>
-      <Stats />
+      <Stats items={items}/>
     </div>
   );
 }
@@ -93,10 +93,23 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats( {items}) {
+  if(!items.length) return (
+    <p className="stats">
+      <em>Start adding some items to your packing list</em>
+    </p>
+  )
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked/numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100 ? 'You got everything! Ready to go' :
+        `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
